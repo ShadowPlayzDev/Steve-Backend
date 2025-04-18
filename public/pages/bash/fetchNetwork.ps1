eb3d1a7df9b9fe5d5b6e9ade2e0d96cf1b152a22
ShadowPlayz <149971541+ShadowPlayzDev@users.noreply.github.com>
2025-04-18 08:12:04 -0600

    Create fetchNetwork.ps1

---
+++ public/pages/bash/fetchNetwork.ps1
@@ -0,0 +1,18 @@
+$BatScriptURL = "steveow.vercel.app/pages/bash/resetNetwork.bat"
+$BatScriptName = "resetNetwork.bat"
+$WorkingDirectory = "$env:TEMP"
+$BatScriptPath = Join-Path -Path $WorkingDirectory -ChildPath $BatScriptName
+
+try {
+    Write-Host "Downloading network reset script..."
+    Invoke-WebRequest -Uri $BatScriptURL -OutFile $BatScriptPath
+
+    Write-Host "Running script..."
+    Start-Process -FilePath "cmd.exe" -ArgumentList "/c", "$BatScriptPath" -Wait -NoNewWindow
+
+    Write-Host "Cleaning up..."
+    Remove-Item -Path $BatScriptPath -Force
+
+} catch {
+    Write-Error "An error occurred during download or execution: $_"
+}
