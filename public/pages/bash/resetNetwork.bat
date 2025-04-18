6e3fd8376f2c139326736ba3bc882adf83a422e0
ShadowPlayz <149971541+ShadowPlayzDev@users.noreply.github.com>
2025-04-18 09:52:46 -0600

    Update resetNetwork.bat

--- public/pages/bash/resetNetwork.bat
+++ public/pages/bash/resetNetwork.bat
@@ -1,11 +1,11 @@
 @echo off
 echo Resetting Network...
-ipconfig /release
-ipconfig /flushdns
-ipconfig /renew
+ipconfig /release > nul 2>&1
+ipconfig /flushdns > nul 2>&1
+ipconfig /renew > nul 2>&1
 echo Renewing IP Address...
-netsh winsock reset
-netsh int ip reset all
+netsh winsock reset > nul 2>&1
+netsh int ip reset all > nul 2>&1
 echo Network reset complete. You may need to restart your computer.
 echo.
 choice /m "Would you like to restart now? (Y/N)" /c YN /n
