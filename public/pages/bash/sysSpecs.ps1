1f0b63913f92297f3f2043ae4836490b58b491e9
ShadowPlayz <149971541+ShadowPlayzDev@users.noreply.github.com>
2025-05-04 09:47:51 -0500

    Create sysSpecs.ps1

---
+++ public/pages/bash/sysSpecs.ps1
@@ -0,0 +1,13 @@
+Write-Host "=== System Information ===`n"
+
+Write-Host "OS:" (Get-CimInstance Win32_OperatingSystem).Caption
+Write-Host "OS Version:" (Get-CimInstance Win32_OperatingSystem).Version
+Write-Host "CPU:" (Get-CimInstance Win32_Processor).Name
+Write-Host "RAM:" ((Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1GB).ToString("0.00") + " GB"
+Write-Host "GPU:" (Get-CimInstance Win32_VideoController).Name
+Write-Host "Disk Drives:" (Get-CimInstance Win32_DiskDrive | ForEach-Object { $_.Model + " - " + [math]::Round($_.Size / 1GB) + " GB" })
+Write-Host "Motherboard:" (Get-CimInstance Win32_BaseBoard | ForEach-Object { $_.Manufacturer + " " + $_.Product })
+Write-Host "System Model:" (Get-CimInstance Win32_ComputerSystem).Model
+Write-Host "Serial Number:" (Get-CimInstance Win32_BIOS).SerialNumber
+Write-Host "Uptime:" ((Get-CimInstance Win32_OperatingSystem).LastBootUpTime)
+Write-Host "IP Address:" (Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.IPAddress -notlike '169.*'} | Select-Object -ExpandProperty IPAddress)
