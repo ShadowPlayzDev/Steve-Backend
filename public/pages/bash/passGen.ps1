36fa3161eceb77c377dfeea4cdeaa502e50f1033
ShadowPlayz <149971541+ShadowPlayzDev@users.noreply.github.com>
2025-05-04 09:59:41 -0500

    Update passGen.ps1

--- public/pages/bash/passGen.ps1
+++ public/pages/bash/passGen.ps1
@@ -6,11 +6,11 @@ param (
 if ($length -match '^\d+$' -and $length -ge 8 -and $length -le 128) {
     $length = [int]$length
 } else {
-    Write-Host "Invalid length specified. Using default length (16)."
+    Write-Output "Invalid length specified. Using default length (16)."
     $length = 16
 }
 
 $characters = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
 $password = -join ((Get-Random -Count $length -InputObject $characters) | ForEach-Object {$_})
 
-Write-Host "Generated Password: $password"
+Write-Output "Generated Password: $password"
