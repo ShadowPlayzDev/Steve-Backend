b233810ad228377058e10eb68ec522e259f57837
ShadowPlayz <149971541+ShadowPlayzDev@users.noreply.github.com>
2025-05-04 10:10:04 -0500

    Update passGen.ps1

--- public/pages/bash/passGen.ps1
+++ public/pages/bash/passGen.ps1
@@ -1,15 +1,12 @@
-$uri = [System.Web.HttpUtility]::ParseQueryString([System.Uri]::EscapeUriString($MyInvocation.MyCommand.Definition))
-$length = $uri["length"]
-if (-not $length) {
-    $length = 16
-}
+param (
+    [string]$length = 16
+)
 if ($length -match '^\d+$' -and $length -ge 8 -and $length -le 128) {
     $length = [int]$length
 } else {
-    Write-Output "Invalid length specified. Using default length (16)."
+    Write-Host "None or Invalid length specified. Using default length (16)."
     $length = 16
 }
-# Define characters used for password
 $characters = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
 $password = -join ((1..$length) | ForEach-Object { $characters | Get-Random })
 
