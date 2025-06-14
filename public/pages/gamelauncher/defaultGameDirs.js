bb6a6fbf73ecdd9f1af2aa2199b54b2f11a88492
Steve <149971541+ShadowPlayzDev@users.noreply.github.com>
2025-06-13 21:04:56 -0500

    Update defaultGameDirs.js

--- public/pages/gamelauncher/defaultGameDirs.js
+++ public/pages/gamelauncher/defaultGameDirs.js
@@ -1,10 +1,11 @@
 export default [
     "%ProgramFiles%/Epic Games/Fortnite/FortniteGame/Binaries/Win64/FortniteClient-Win64-Shipping.exe",
     "%ProgramFiles(x86)%/Steam/steamapps/common/Cyberpunk 2077/bin/x64/Cyberpunk2077.exe",
-    "%LOCALAPPDATA%/Roblox/Versions/*/RobloxPlayerBeta.exe", // Example with wildcard for Roblox
+    "%LOCALAPPDATA%/Roblox/Versions/*/RobloxPlayerBeta.exe",
     "%ProgramFiles%/Genshin Impact/Genshin Impact Game/GenshinImpact.exe",
     "%ProgramFiles(x86)%/Battle.net/Overwatch/Overwatch.exe",
-// Launchers
+
+
     "%ProgramFiles%/Riot Games/Riot Client/RiotClientServices.exe",
     "%ProgramFiles(x86)%/Riot Games/League of Legends/LeagueClient.exe",
     "%ProgramFiles(x86)%/Battle.net/Battle.net Launcher.exe",
